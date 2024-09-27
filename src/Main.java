5e8c6a95fbaac9a531e86500cc7efa3f1cb5fa8b
steph <stephenbenny050@gmail.com>
2024-09-27 15:23:22 -0400

    Task 4 lab 5

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+            Scanner console = new Scanner(System.in);
+    {
+
+        System.out.println("Enter your age:");
+        int age = console.nextInt();
+        if (age >= 21)
+        {
+            System.out.println("You get a wristband! ");
+        }
+         }
+        }
+    }
